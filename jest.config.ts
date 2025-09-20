86065c9d799f07786d8eb7730113d8face0e0cb3
anntnzrb <anntnzrb@proton.me>
2025-09-20 18:37:04 -0500

    Fix Jest config for Node.js 22 compatibility

--- jest.config.ts
+++ jest.config.ts
@@ -3,8 +3,6 @@
  */
 
 import type { Config } from 'jest';
-import { pathsToModuleNameMapper } from 'ts-jest';
-import { compilerOptions } from './tsconfig.json';
 
 const config: Config = {
   preset: 'ts-jest',
@@ -12,9 +10,15 @@ const config: Config = {
   roots: ['<rootDir>/src', '<rootDir>/tests'],
   moduleFileExtensions: ['ts', 'js'],
   clearMocks: true,
-  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths ?? {}, {
-    prefix: '<rootDir>/',
-  }),
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '^@controllers/(.*)$': '<rootDir>/src/controllers/$1',
+    '^@models/(.*)$': '<rootDir>/src/models/$1',
+    '^@routes/(.*)$': '<rootDir>/src/routes/$1',
+    '^@validators/(.*)$': '<rootDir>/src/validators/$1',
+    '^@views/(.*)$': '<rootDir>/src/views/$1',
+    '^@core/(.*)$': '<rootDir>/src/core/$1',
+  },
 };
 
 export default config;
